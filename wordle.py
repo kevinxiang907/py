717514ff08b35e85b862e928cb431a2597c68a9e
kevinxiang907 <kevin71390xiang@gmail.com>
2022-10-14 18:29:10 +0800

    add import random

--- wordle.py
+++ wordle.py
@@ -1,14 +1,16 @@
+import random
+
 if __name__ == "__main__":
     # User input
     user_input = input()
 
 
     # Specify an answer.
-    f = open("words.text", "r")
+    f = open("words.txt", "r")
     dictionary = f.read().splitlines()
     f.close()
 
-    answer = random.sample(directionae, 1)[0]
+    answer = random.sample(dictionary, 1)[0]
     print(answer)
 
     if not user_input in dictionary:
@@ -19,7 +21,8 @@ if __name__ == "__main__":
     for i in range(len(user_input)):
         if user_input[i] == answer[i]:
             print("A")
-            elif user_input[i] in answer:
-                print("B")
-            else:
-                print("X")
+        elif user_input[i] in answer:
+            print("B")
+        else:
+            print("X")
+    
