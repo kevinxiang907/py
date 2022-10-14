305a1f1a0964b965cdf8beac68f5ac28b3fa825c
kevinxiang907 <kevin71390xiang@gmail.com>
2022-10-14 17:51:14 +0800

    add a checked input

--- wordle.py
+++ wordle.py
@@ -2,6 +2,7 @@ if __name__ == "__main__":
     # User input
     user_input = input()
 
+
     # Specify an answer.
     f = open("words.text", "r")
     dictionary = f.read().splitlines()
@@ -9,6 +10,10 @@ if __name__ == "__main__":
 
     answer = random.sample(directionae, 1)[0]
     print(answer)
+
+    if not user_input in dictionary:
+        print("Please input an valid word")
+        exit()
 
     # Compare user input and answer
     for i in range(len(user_input)):
